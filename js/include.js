c457d66817c8ca22f6bc29d8efc93ba4c48813f7
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2026-02-04 22:01:05 +0900

    update-02　JSの修正

--- js/include.js
+++ js/include.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('../parts/header.html')
+  fetch('parts/header.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('site-header').innerHTML = html;
     });
 
-  fetch('../parts/footer.html')
+  fetch('parts/footer.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('site-footer').innerHTML = html;
