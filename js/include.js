cb0a7e561957717ca403752167a6618ea30d16d6
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2026-02-10 13:27:48 +0900

    update-11　再

--- js/include.js
+++ js/include.js
@@ -11,6 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('availability').innerHTML = html;
     });
 
+  fetch('parts/line.html')
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('line-bottom').innerHTML = html;
+    });
+
   fetch('parts/footer.html')
     .then(res => res.text())
     .then(html => {
