e477e4960e15406e311061dab0a5742acc7f388d
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2026-08-31 10:04:02 +0900

    最新版の更新
    2026 / 8 / 31 時点での最新版データです。

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,37 @@
-const modalBox = document.querySelector('.modal-box');
-const modalBg = modalBox.querySelector('.modal-bg');
-const modalCloseBtn = modalBox.querySelector('.close');
-// const triggerTarget = document.querySelector('.modal-trigger-target');
+// const modalBox = document.querySelector('.modal-box');
+// const modalBg = modalBox.querySelector('.modal-bg');
+// const modalCloseBtn = modalBox.querySelector('.close');
 
-let hasShownModal = false; // ★表示済みかどうかのフラグ
+// let hasShownModal = false; // ★表示済みかどうかのフラグ
 
-function openGeneralModal() {
-  if (!hasShownModal) {
-    modalBox.classList.add('show');
-    hasShownModal = true; // ★1回表示したらフラグをON
-    document.body.style.overflow = "hidden";
-  }
-}
+// function openGeneralModal() {
+//   if (!hasShownModal) {
+//     modalBox.classList.add('show');
+//     hasShownModal = true; // ★1回表示したらフラグをON
+//     document.body.style.overflow = "hidden";
+//   }
+// }
 
-function closeGeneralModal() {
-  modalBox.classList.remove('show');
-  document.body.style.overflow = "";
-}
+// function closeGeneralModal() {
+//   modalBox.classList.remove('show');
+//   document.body.style.overflow = "";
+// }
 
 
-// スクロール発火位置
-const triggerScroll = 200;
+// // スクロール発火位置
+// const triggerScroll = 200;
 
-window.addEventListener('scroll', () => {
+// window.addEventListener('scroll', () => {
 
-  const scrollY = window.scrollY;
+//   const scrollY = window.scrollY;
 
-  if (scrollY > triggerScroll && !hasShownModal) {
-    openGeneralModal();
-  }
+//   if (scrollY > triggerScroll && !hasShownModal) {
+//     openGeneralModal();
+//   }
 
-});
+// });
 
-// 閉じる処理（背景クリックや×ボタン）
-modalBg.addEventListener('click', closeGeneralModal);
-modalCloseBtn.addEventListener('click', closeGeneralModal);
+// // 閉じる処理（背景クリックや×ボタン）
+// modalBg.addEventListener('click', closeGeneralModal);
+// modalCloseBtn.addEventListener('click', closeGeneralModal);
 
